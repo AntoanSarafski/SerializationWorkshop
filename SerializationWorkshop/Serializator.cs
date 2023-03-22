b2d51db4b803af86c72db88d03961e456b58e9ab
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-22 14:47:03 +0200

    Commit serializator with Save method.

---
+++ SerializationWorkshop/Serializator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SerializationWorkshop
+{
+    public class Serializator
+    {
+        public void Save(object obj)
+        {
+            Type type = obj.GetType();
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (var prop in type.GetProperties())
+            {
+                result.Append($"{prop.Name}:{prop.GetValue(obj)}|:|");
+
+            }
+            using (StreamWriter writer = new StreamWriter($"../../../{type.Name}"))
+            {
+                writer.Write(writer.ToString());
+            }
+        }
+    }
+}
